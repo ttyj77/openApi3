0d830ad2d22370e16002c3fd41e543093799f602
jinujin <espin97@naver.com>
2023-04-25 15:39:03 +0900

    04/25 api details start

--- src/main/resources/static/js/details.js
+++ src/main/resources/static/js/details.js
@@ -24,8 +24,40 @@ window.onload = function () {
 
 // **** 외부 function 호출 zone ****
 function selectChange() {
+    let resource = "resource";
+    let tag = "tag";
     let select = document.getElementById('show');
     selectNowName = select.options[select.selectedIndex].text;
+    let selectNowValue = select.options[select.selectedIndex].value;
+    let resourceBtn = $(document.createElement("button")).addClass("btn btn-secondary").attr({
+        "id":"resourceBtn",
+        "data-bs-toggle": "modal",
+        "data-bs-target": "#resourceModal"
+    }).text(plusIcon + selectNowName + addContext);
+    let tagBtn = $(document.createElement("button")).addClass("btn btn-secondary").attr({
+        "id":"tagBtn",
+        "data-bs-toggle": "modal",
+        "data-bs-target": "#tagModal"
+    }).text(plusIcon + selectNowName + addContext);
+
     var pickBtn = document.getElementById('pick');
-    pickBtn.innerHTML = plusIcon + selectNowName + addContext;
+
+    if (selectNowValue == resource) {
+        $('#target').append(resourceBtn);
+        $('#resourceBtn').click();
+    } else if (selectNowValue == tag) {
+        $('#target').append(tagBtn);
+        $('#tagBtn').click();
+    }
+}
+
+function addTag() {
+    // 태그 추가 코드~~
+
+    Swal.fire({
+        title: '등록 성공',
+        icon: 'success'
+    }).then(() => {
+        $('#tagModal').modal('hide');
+    })
 }
